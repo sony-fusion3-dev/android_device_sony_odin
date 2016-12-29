608e50866d4327e4e01c8fac4377664ecb7ffc40
Markus Menzel <markusmenzel@gmail.com>
2016-12-29 13:29:50 +0000

    Rename defconfig
    Change-Id: I5315b2232ddcd8213565ccb530a1689a8189405c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/odin/include
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/odin/bluetooth
 
-TARGET_KERNEL_CONFIG := cm_fusion3_odin_defconfig
+TARGET_KERNEL_CONFIG := fusion3_odin_defconfig
 
 # Healthd
 BACKLIGHT_PATH := /sys/class/leds/lm3533-lcd-bl-1/brightness

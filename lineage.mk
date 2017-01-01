0051ef33245dac1d6346d55531ec11302cdf2adb
Kalpaj Chaudhari <daedroza@gmail.com>
2017-01-01 19:55:27 +0530

    odin: Rebrand to LineageOS
    Change-Id: If3bc89a3b10f474180152daa23b244a8ab9a157b

--- lineage.mk
+++ lineage.mk
@@ -5,5 +5,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503/C6503:5.1.1/10.7.A.0.222/4076033025:user/release-keys PRIVATE_BUILD_DESC="C6503-user 5.1.1 10.7.A.0.222 4076033025 test-keys"
 
-PRODUCT_NAME := cm_odin
+PRODUCT_NAME := lineage_odin
 PRODUCT_DEVICE := odin

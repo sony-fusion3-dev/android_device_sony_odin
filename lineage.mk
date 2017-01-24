4022d4a80e97528936b87e6e8962803acb3359d5
Roman Yarullin <firefox883@gmail.com>
2017-01-24 18:11:41 +0000

    odin: update build fingerprint & description
    * From 10.7.A.0.228
    
    Change-Id: If8ac09738449b20675df0503bcdbfc54fd68ac68

--- lineage.mk
+++ lineage.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/sony/odin/full_odin.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503/C6503:5.1.1/10.7.A.0.222/4076033025:user/release-keys PRIVATE_BUILD_DESC="C6503-user 5.1.1 10.7.A.0.222 4076033025 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6503 BUILD_FINGERPRINT=Sony/C6503/C6503:5.1.1/10.7.A.0.228/3219080026:user/release-keys PRIVATE_BUILD_DESC="C6503-user 5.1.1 10.7.A.0.228 3219080026 release-keys"
 
 PRODUCT_NAME := lineage_odin
 PRODUCT_DEVICE := odin
